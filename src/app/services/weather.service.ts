eb9eed37af41dcdcb3f73487d8b7996d8fd5a6ca
Rajat Khare <rajat.khare@pb.com>
2021-08-09 00:28:40 +0530

    Error handling

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -10,18 +11,42 @@ export class WeatherService {
   constructor(private http: HttpClient) {}
 
   getCityWeatherData(city: string, country: string): Observable<WeatherData> {
-    return this.http.get<WeatherData>(
-      `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&units=metric&appid=${this.API_KEY}`
-    );
+    return this.http
+      .get<WeatherData>(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&units=metric&appid=${this.API_KEY}`
+      )
+      .pipe(
+        catchError((err) => {
+          return throwError(err);
+        })
+      );
   }
 
   getRandomWeatherData(): Observable<WeatherData> {
     const lon = +(Math.random() * 180).toFixed(3);
     const lat = +(Math.random() * 90).toFixed(3);
 
-    return this.http.get<WeatherData>(
-      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${this.API_KEY}`
-    );
+    return this.http
+      .get<WeatherData>(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${this.API_KEY}`
+      )
+      .pipe(
+        catchError((err) => {
+          return throwError(err);
+        })
+      );
+  }
+
+  getWeatherDataWithCoords(lat: number, lon: number) {
+    return this.http
+      .get<WeatherData>(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${this.API_KEY}`
+      )
+      .pipe(
+        catchError((err) => {
+          return throwError(err);
+        })
+      );
   }
 }
 
